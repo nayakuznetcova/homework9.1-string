70acac74312b6dacbf07245d514873b8bc9ba17d
Admin <samedi450511@gmail.com>
2023-11-04 13:11:11 +0600

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String probel = " ";
+        String fullName = lastName + probel + firstName + probel +middleName;
+        System.out.println("Ф.И.О. сотрудника: " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String probel = " ";
+        String fullName = lastName + probel + firstName + probel +middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String firstName2 = firstName.replace('ё', 'е');
+        String middleName2 = middleName.replace('ё', 'е');
+        String fullName = lastName + " " + firstName2 + " " +middleName2;
+        System.out.println("Ф.И.О. сотрудника: " + fullName);
     }
 }
